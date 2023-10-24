3057eb4cf2b724e620e9bd06f76656ed241cbb97
Will <willbickham3@gmail.com>
2023-10-23 23:58:17 -0500

    Function that deletes songs from respective directory

---
+++ delete_songs.py
@@ -0,0 +1,11 @@
+import os
+
+directory_path = 'songs'
+
+def delete_songs():
+    for files in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, files)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+delete_songs()
