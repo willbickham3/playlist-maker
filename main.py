db6836b652980d0b38f49b3ec94973be8d00ddec
Will <willbickham3@gmail.com>
2023-10-29 01:28:11 -0500

    Import functions

---
+++ main.py
@@ -0,0 +1,9 @@
+#from delete_songs import delete_songs
+from video_download import download_video
+from song import create_song
+
+download_video("https://www.youtube.com/watch?v=8Vs3hhEBFYg")
+
+#create_song("Synthwave radio (EXCLUSIVE SONGS) 🌌 - beats to chillgame tostudy.mp4", "hhi", 0, 120)
+#create_song("downloaded/WoW Synthwave Beats to Chill To  Journey to BlizzCon.mp4", "orcie", 0, 274)
+#delete_songs()
