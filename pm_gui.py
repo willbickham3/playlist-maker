e8da642b1702ef96344109c96c524a5a6fcca07c
Will <willbickham3@gmail.com>
2023-11-01 02:31:35 -0500

    Begin building a gui

---
+++ pm_gui.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+import tkinter as tk
+from video_download import download_video
+
+layout = [
+    [sg.Text("Build Your Own Playlist!")],
+    [sg.InputText(key="user_input", size=(43, 1.5))],
+    [sg.Text("Enter a start time and end time for your clip in seconds.")],
+    [sg.InputText(key="user_start", size=(5, 1.5), enable_events=True, default_text="Start"),],
+    [sg.InputText(key="user_end", size=(5, 1.5), default_text="End")],
+    [sg.Button("Submit")]
+]
+
+window = sg.Window(title="Playlist Builder", layout=layout, margins=(200, 200))
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+    if event == "Submit":
+        user_input = values["user_input"]   #Retrieves user input
+        download_video(user_input)
+        sg.popup("You pressed the button!")
+
+    if event == "user_start" and values["user_start"] == "Start":
+        sg.popup('hi')
+        window["user_start"].update(value="")
+
+    if values["user_end"] == "End":
+        window["user_end"].update(value="")
+
+window.close()
