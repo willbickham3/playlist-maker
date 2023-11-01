405252d837f10165e8ac0e8579c1a462cdf70f62
Will <willbickham3@gmail.com>
2023-11-01 02:31:14 -0500

    Add error handling for different scenarios

--- video_download.py
+++ video_download.py
@@ -1,21 +1,41 @@
 from pytube import YouTube
 import shutil
+from pytube.exceptions import VideoUnavailable, VideoPrivate, RegexMatchError
 
 def download_video(url):
     """Takes in a url in string form and downloads the highest quality sound from the video"""
+    
+    url = f"{url}"
+
+    if "youtube.com/watch?v=" not in url:
+        print("URL is not from YouTube. Please provide a YouTube URL.")
+        return
+    
+
     yt = YouTube(url)
-    audio_files = yt.streams.filter(file_extension="mp4")
-    bitrate_list = []
-    for x in audio_files:
-        if x.abr == None:
-            continue
-        bitrate = x.abr.split('kbps')
-        bitrate_list.append(int(bitrate[0]))
-    bitrate_list.sort(reverse=True)
-    highest_bitrate = f"{bitrate_list[0]}kbps"
 
+    if yt.title in "downloaded":
+        print("You already got it!")
+    try:
+        if yt.streams:
+            audio_files = yt.streams.filter(file_extension="mp4")
+            bitrate_list = []
+            for x in audio_files:
+                if x.abr == None:
+                    continue
+                bitrate = x.abr.split('kbps')
+                bitrate_list.append(int(bitrate[0]))
+            bitrate_list.sort(reverse=True)
+            highest_bitrate = f"{bitrate_list[0]}kbps"
+            for y in audio_files:
+                if y.abr == highest_bitrate:
+                    file_to_download = audio_files.get_by_itag(y.itag)
+                    shutil.move(file_to_download.download(), "downloaded")
+        else:
+            raise Exception("No streamable content found in the provided URL.")
+    except VideoPrivate:
+        print("Video is private and cannot be downloaded.")
+    except VideoUnavailable:
+        print("Video is unavailable or restricted.")
 
-    for y in audio_files:
-        if y.abr == highest_bitrate:
-            file_to_download = audio_files.get_by_itag(y.itag)
-            shutil.move(file_to_download.download(), "downloaded")
+download_video("Bep")
