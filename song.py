76fe3c67c79030c443ef3fc9791e0f2b53e183b4
Will <willbickham3@gmail.com>
2023-11-03 23:35:39 -0500

    Asks for a song name before creating the clip

--- song.py
+++ song.py
@@ -2,7 +2,9 @@ from pytube import YouTube
 from moviepy.editor import *
 import shutil
 
-def create_song(input_path, name, start, end):
+def create_song(input_path, start, end):
+    print("Please enter a name for your song/clip.")
+    name = str(input())
     song = VideoFileClip(f"{input_path}").subclip(start, end)
     song = song.audio
     output_file = f"{name}.mp3"
